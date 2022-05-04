26c04f2f36c3066a7ee56d0cf25f208c5c74a121
jvictorpereira <102594213+jvictorpereira@users.noreply.github.com>
2022-05-04 09:32:35 -0300

    Create style.css

---
+++ style.css
@@ -0,0 +1,165 @@
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    color: white;
+    font-family: 'Montserrat', sans-serif;
+}
+ul{
+    list-style: none;
+
+    font-style: italic;
+    
+
+}
+a{
+    text-decoration: none;
+    font-weight: 700;
+}
+
+body{
+
+    background: url(./background.jpg) no-repeat center center fixed;
+    background-size: cover;
+}
+
+header{
+    display: flex;
+    justify-content: space-around  ;
+    align-items: center;
+    padding: 10px;
+   
+}
+
+header ul{
+    display: flex;
+
+
+
+}
+
+header ul li{
+    
+    padding: 8px;
+    border-bottom: 1px dashed #fff;
+
+}
+header ul li a{
+    transition: 300ms;
+}
+
+header ul li a:hover{
+    color: gray;
+
+}
+
+.container {
+    text-align: center;
+    position: relative;
+    bottom: -5cm;
+}
+.container .name h1{
+    font-size: 60px;
+}
+
+.container .name h5{
+    font-size: 25px;
+
+}
+
+#arrow{
+font-size: 30px;
+cursor: pointer;
+animation: blink 1s steps(5, start) infinite;
+
+}
+@keyframes blink {
+    to {
+        visibility: hidden;
+    }
+}
+#animation {
+    transition: 300ms;
+}
+.rotate {
+    transform: rotate(180deg);
+    transition: 300ms
+}
+
+.active {
+    display: block !important;
+    transition: 300ms;
+
+}
+
+
+#about p{
+    max-width: 600px;
+    margin: 0 auto;
+    display: flex;
+    
+  
+
+}
+
+
+.form {
+    width: 50%;
+    margin: 0 auto ;
+    position: relative;
+    bottom: -9cm;
+    
+    
+    
+    
+}
+
+ .form input{ 
+    width: 50%;
+    padding: 5px 5px;
+    background-color: rgb(26, 23, 23);
+    border: none;
+    border-radius: 5px 0px 0px 5px;
+
+}
+
+
+ .form button{
+    width: 15%; 
+    padding: 5px 5px;
+    background-color: gray;
+    color: black;
+    font-weight: 600;
+    border: none;
+    cursor: pointer;
+    transition: 300ms;
+    border-radius: 0px 5px 5px 0px;
+    margin-left: -5px;
+    margin-top: 15px;
+}
+.form button:hover{
+    background-color: gray;
+    color: #fff;
+    
+}
+@media only screen and (max-width: 768px){
+    .form{
+        width: 100%; ;
+    }
+
+}
+
+@media only screen and (max-width: 400px) {
+    .container .name h1{
+        font-size: 30px;
+
+    }
+    .container .name h5{
+        font-size: 15px;
+    }
+    #about p{
+        max-width: 80%;
+        text-align: justify;
+    }
+
+}
